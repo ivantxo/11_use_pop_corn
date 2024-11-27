98ae16da93b0392d2e6687646f552295def9aa7a
IvanR <ivan.ramirezf@gmail.com>
2024-11-28 00:24:15 +1000

    Extracting Search component

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,21 +1,14 @@
 import { useState } from "react";
+import Search from "./Search";
 
 function NavBar() {
-  const [query, setQuery] = useState("");
-
   return (
     <nav className="nav-bar">
       <div className="logo">
         <span role="img">🍿</span>
         <h1>usePopcorn</h1>
       </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+      <Search />
       <p className="num-results">
         Found <strong>X</strong> results
       </p>
---
+++ src/Search.js
@@ -0,0 +1,15 @@
+function Search() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+export default Search;
