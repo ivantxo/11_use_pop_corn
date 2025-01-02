747e607f63dadc80b01fde817319437e71a99f0b
IvanR <ivan.ramirezf@gmail.com>
2025-01-03 01:04:46 +1000

    Gain focus when hitting Enter key
    - If element already has focus and press Enter again, it shouldn't do
    anything

--- src/Search.js
+++ src/Search.js
@@ -3,9 +3,21 @@ import { useEffect, useRef } from "react";
 function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    inputEl.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   //   useEffect(function () {
   //     const el = document.querySelector(".search");
